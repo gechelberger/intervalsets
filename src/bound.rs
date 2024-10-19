a1f343c60ff82a5fcd2848f2ef3e2f2fe4019a92
Greg Echelberger <gechelberger@gmail.com>
2024-10-19 22:18:18 +0000

    docs: [resolves #72] docstrings for Bound

--- src/bound.rs
+++ src/bound.rs
@@ -18,6 +18,9 @@ impl Side {
     }
 }
 
+/// The BoundType determines the inclusivity of the limit element in a set.
+///
+/// `Closed` bounds include the limit value in the `Set`, `Open` bounds do not.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub enum BoundType {
     Closed,
@@ -41,6 +44,7 @@ impl BoundType {
 pub struct Bound<T>(BoundType, T);
 
 impl<T: Clone + PartialOrd> Bound<T> {
+    /// Returns the smaller `Bound`, treating args as left hand (lower) bounds.
     pub fn min_left(a: &Self, b: &Self) -> Self {
         if a.contains(Side::Left, b.value()) {
             a.clone()
@@ -49,6 +53,7 @@ impl<T: Clone + PartialOrd> Bound<T> {
         }
     }
 
+    /// Returns the smaller `Bound`, treating args as right hand (upper) bounds.
     pub fn min_right(a: &Self, b: &Self) -> Self {
         if a.contains(Side::Right, b.value()) {
             b.clone()
@@ -57,6 +62,7 @@ impl<T: Clone + PartialOrd> Bound<T> {
         }
     }
 
+    /// Returns the larger `Bound`, treating args as left hand (lower) bounds.
     pub fn max_left(a: &Self, b: &Self) -> Self {
         if a.contains(Side::Left, b.value()) {
             b.clone()
@@ -65,6 +71,7 @@ impl<T: Clone + PartialOrd> Bound<T> {
         }
     }
 
+    /// Returns the larger `Bound`, treating args as right hand (upper) bounds.
     pub fn max_right(a: &Self, b: &Self) -> Self {
         if a.contains(Side::Right, b.value()) {
             a.clone()
@@ -73,40 +80,60 @@ impl<T: Clone + PartialOrd> Bound<T> {
         }
     }
 
+    /// Return a new `Bound` with the same limit, but flipped `BoundType`.
     pub fn flip(&self) -> Self {
         Self(self.0.flip(), self.1.clone())
     }
 }
 
 impl<T> Bound<T> {
+    /// Returns a new closed `Bound` constrained at `limit`.
     pub fn closed(limit: T) -> Self {
         Self(BoundType::Closed, limit)
     }
 
+    /// Returns a new open `Bound` constrained at `limit`.
     pub fn open(limit: T) -> Self {
         Self(BoundType::Open, limit)
     }
 
+    /// Returns a new `Bound`, retaining BoundType, with a new limit.
+    ///
+    /// # Examples
+    /// ```
+    /// use intervalsets::Bound;
+    /// let bound = Bound::closed(10);
+    /// let shift = bound.map(|limit| limit + 10);
+    /// assert_eq!(shift, Bound::closed(20));
+    ///
+    /// let float = bound.map(|limit| *limit as f32);
+    /// assert_eq!(float, Bound::closed(10.0));
+    /// ```
     pub fn map<U>(&self, func: impl FnOnce(&T) -> U) -> Bound<U> {
         Bound::<U>(self.0, func(self.value()))
     }
 
+    /// Returns `true` if this bound's `BoundType` is `Open`.
     pub fn is_open(&self) -> bool {
         matches!(self.0, BoundType::Open)
     }
 
+    /// Returns `true` if this bound's `BoundType` is `Closed`
     pub fn is_closed(&self) -> bool {
         matches!(self.0, BoundType::Closed)
     }
 
+    /// Return the `BoundType` of this `Bound`.
     pub fn bound_type(&self) -> BoundType {
         self.0
     }
 
+    /// Returns a reference to this bound's limit value.
     pub fn value(&self) -> &T {
         &self.1
     }
 
+    /// Map a binary operation over `Bound<T>`. `self` is consumed.
     pub fn binary_map(self, func: impl Fn(T, T) -> T, rhs: T) -> Self {
         Self(self.0, func(self.1, rhs))
     }
